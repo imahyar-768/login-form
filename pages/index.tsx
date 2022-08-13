1bbad0fcaeb42789419fed015c12cead8857b254
nikdast <mahyar768@gmail.com>
2022-08-13 11:48:59 +0430

    feat/create form and submit data

---
+++ pages/index.tsx
@@ -0,0 +1,19 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import LoginForm from '../components/login-form'
+
+const Home: NextPage = () => {
+  return (
+    <div>
+      <Head>
+        <title>Login Page</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main className='vh-100 d-flex justify-content-center align-items-center'>
+        <LoginForm/>
+      </main>
+    </div>
+  )
+}
+
+export default Home
