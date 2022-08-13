24394fca54f2b663289a55256413823c5cc7d5ad
nikdast <mahyar768@gmail.com>
2022-08-13 12:15:43 +0430

    feat/add validation form with yup

--- components/login-form.tsx
+++ components/login-form.tsx
@@ -1,10 +1,19 @@
 import {Formik, Field, Form, FormikHelpers} from 'formik';
+import * as Yup from 'yup';
 import styles from './login-form.module.css';
 
 interface Values {
-    username: string;
+    email: string;
     password: string;
 }
+
+const SignupSchema = Yup.object().shape({
+    email: Yup.string().email('Invalid email').required('Required'),
+    password: Yup.string()
+     .required('Required') 
+     .min(8, 'Password is too short - should be 8 chars minimum.')
+     .matches(/[a-zA-Z]/, 'Password can only contain Latin letters.')
+})
 
 const LoginForm = () => {
     return (
@@ -12,9 +21,10 @@ const LoginForm = () => {
         <h1 className='display-6 mb-3'>Login</h1>    
         <Formik 
             initialValues = {{
-                username: '',
+                email: '',
                 password: '',
-            }}    
+            }}
+            validationSchema={SignupSchema}    
             onSubmit = {(
                 values: Values,
                 { setSubmitting }: FormikHelpers<Values>
@@ -26,16 +36,19 @@ const LoginForm = () => {
                 
             }}
         >
-            <Form>
-                <div className='mb-3'>
-                    <Field id="username" name="username" placeholder="Username" />
-                </div>
-                <div className='mb-3'>
-                    <Field id="password" name="password" placeholder="Password" />
-                </div>
-                <button type="submit" className='btn btn-primary'>Login</button>
-            </Form>
-            
+            {({ errors, touched }) => (
+                <Form>
+                    <div className='mb-3'>
+                        <Field id="email" name="email" type='email' placeholder="Email" />
+                        {errors.email && touched.email ? <div className={styles.error}>{errors.email}</div> : null}
+                    </div>
+                    <div className='mb-3'>
+                        <Field id="password" name="password" placeholder="Password" />
+                        {errors.password && touched.password ? (<div className={styles.error}>{errors.password}</div>) : null}
+                    </div>
+                    <button type="submit" className='btn btn-primary'>Login</button>
+                </Form>
+            )}
         </Formik>
         </div>
     )
